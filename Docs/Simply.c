67750f572a3eb595a2725486eb5a87bb6e51e2ac
deeptir96 <deeptir96@gmail.com>
2016-02-18 19:00:28 +0530

    first commit

---
+++ Docs/Simply.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("This is just for demonstration purposes\n");
+	//Feel free to edit, call blxhaildhzkshvk
+
+	return 0;
+
+	//Bye bye
+}
